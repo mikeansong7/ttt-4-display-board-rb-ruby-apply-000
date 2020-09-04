b1f64795a2b39659b2df0106f3d94998c185d89d
Michael B Ansong <michael.ansong7@gmail.com>
2020-09-04 19:45:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 
 def display_board(board)
-  board= (" ", " ", " ", "  ", " ", " ", "  ", " ", " ")
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -8,7 +7,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end 
 
- display_board(board)
+ 
 
 
 
