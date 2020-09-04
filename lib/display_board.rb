5e60172f04707f5e4634a5f7dc7237e76a704155
Michael B Ansong <michael.ansong7@gmail.com>
2020-09-04 19:23:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,16 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   " 
-end 
-
-board= (" ", " ", " ", "  ", " ", " ", "  ", " ", " ")
-
 def display_board(board)
   puts "  #{board[0]} |#{board[1]}  |#{board[2]} "
   puts "-----------"
@@ -19,7 +9,7 @@ def display_board(board)
   puts "  #{board[6]} |#{board[7]}  |#{board[8]} "
 end 
 
-display_board(board)
+
 
 
 
